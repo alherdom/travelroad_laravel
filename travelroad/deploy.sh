de959824cce8db09d1a385bb59d173c28c31db50
alherdom <alherdom@outlook.com>
2024-01-08 16:41:09 +0000

    Changes

--- travelroad/deploy.sh
+++ travelroad/deploy.sh
@@ -1,5 +1,13 @@
+#!/bin/bash
+
+cd /usr/share/nginx/travelroad_laravel
+sudo git add .
+sudo git commit -m "Changes"
+sudo git push
+
 ssh alejandrohernandez@alejandrohernandez.arkania.es "
 	cd /home/alejandrohernandez/travelroad_laravel
 	git pull
+	cd travelroad
 	composer install
 	"
